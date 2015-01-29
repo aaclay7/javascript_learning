852316ec8071df80c54c7971679a27093ac90a9f
Clayton <aaclay7@jomc7885.dhcp.unc.edu>
2015-01-29 08:37:01 -0500

    adding files from class

---
+++ jsstyle2.js
@@ -0,0 +1,23 @@
+//this file is used for learning JaveScript.
+    
+    var userName = "Alex";
+    
+    var todaysDate = new Date();
+    
+    function myFunction(){
+         document.write("Hello ");
+         document.write(userName);
+    }
+    
+   
+    document.getElementById("demo").innerHTML="This is the new info";
+    document.getElementById("name").innerHTML="Alex is my name";
+    myFunction();
+    
+    var myNumberVariable = 5;
+    var myTextVariable = "ello mate!";
+    var myDateVariable = new Date();
+    console.log(myNumberVariable);
+    console.log(myTextVariable);
+    console.log(todaysDate);
+    
